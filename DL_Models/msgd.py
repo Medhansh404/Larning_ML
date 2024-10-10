b60c978797325f29ff67871f928549f29620e90b
Medhansh404 <21ucs126@lnmiit.ac.in>
2024-10-11 02:02:09 +0530

    day3

--- DL_Models/msgd.py
+++ DL_Models/msgd.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-X = [0.5, 1.0,  2.5]
+
+X = [0.5, 1.0, 2.5]
 Y = [0.2, 0.84, 0.9]
 
 
@@ -9,33 +10,39 @@ def f(x, w, b):
 
 
 def error(w, b):
-    err = 0.0
-    for x, y in zip(X, Y):
+    err = 0
+    for x,y in zip(X,Y):
         fx = f(x, w, b)
         err += (fx - y) ** 2
     return 0.5 * err
 
 
-def grad_b(x, w, b, y):
+def grad_w(x, y, w, b):
     fx = f(x, w, b)
-    return (fx - y) * fx * (1 - fx)
+    return (fx - y) * (1 - fx) * fx * x
 
 
-def grad_w(x, w, b, y):
+def grad_b(x, y, w, b):
     fx = f(x, w, b)
-    return (fx - y) * fx * (1 - fx) * x
+    return (fx - y) * (1 - fx) * fx
 
 
-def do_sgd():
-    w, b, n, max_epoch = -2, -2, 1.0, 1000
+def do_msgd():
+    max_epochs = 1000
+    w, b, beta, eta = -2, -2, 0.9, 1.0
+    prev_wu, prev_bu = 0, 0
     errors, epochs = [], []
-    for i in range(max_epoch):
+    for i in range(max_epochs):
         dw, db = 0, 0
         for x, y in zip(X, Y):
-            dw += grad_w(x, w, b, y)
-            db += grad_b(x, w, b, y)
-            w = w - n * dw
-            b = b - n * db
+            dw = grad_w(x, y, w, b)
+            db = grad_b(x, y, w, b)
+            uw = prev_wu * beta + eta * dw
+            ub = prev_bu * beta + eta * db
+            w = w - uw
+            b = b - ub
+            prev_wu = uw
+            prev_bu = ub
 
         current_error = error(w, b)
         errors.append(current_error)
@@ -57,4 +64,4 @@ def do_sgd():
 
 
 if __name__ == "__main__":
-    do_sgd()
+    do_msgd()
