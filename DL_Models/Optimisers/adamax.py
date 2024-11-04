94d4e8f4225fcb0bd8ac16a9bc078f695769df26
Medhansh404 <21ucs126@lnmiit.ac.in>
2024-11-04 20:44:40 +0530

    optimisers almost done.... next up making a workflowlet's see

--- DL_Models/Optimisers/adamax.py
+++ DL_Models/Optimisers/adamax.py
@@ -2,4 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 X = [0.2, 0.5, 0.9]
-Y = []
+Y = [0.2, 0.84, 0.99]
+
+
+def f(x, w, b):
+    return 1 / (1+ np.exp(-(w*x +b)))
+
+
+def error(w, b):
+    err = 0
+    for (x,y) in zip(X, Y):
+        fx = f(x, w, b)
+        err += (fx-y) **2
+    return err
+
+
+def grad_w(x, y, w, b):
+    fx = f(x, y, w, b)
+    return (fx-y) * (1-x) * x * fx
+
+
+def grad_b(x, y, w, b):
+    fx = f(x, y, w, b)
+    return (fx-y) * (1-x) * fx
+
+
+def do_adamax():
