4157206a121d56a371dd64e1db15f99b395d9969
Medhansh404 <21ucs126@lnmiit.ac.in>
2024-11-09 00:00:44 +0530

    tweaking

--- DL_Models/Library/__init__.py
+++ DL_Models/Library/__init__.py
@@ -1,9 +1,10 @@
 import argparse as arg
-import Optimisers as optim
+import core.process as process
 
 # input: the input i form an array
 # opt: optimiser
 #
+
 
 def parse_arg():
     parser = arg.ArgumentParser(description='Welcome')
@@ -16,18 +17,21 @@ def parse_arg():
     args = parser.parse_args()
     l_arg = args.numbers
     l = int(l_arg[0])
-    input = l_arg[1:2*l+1]
+    inputs = l_arg[1:2*l+1]
     opt = l_arg[2*l + 1]
     eta = l_arg[2*l + 2]
     epochs = l_arg[2*l + 3]
     b_size = l_arg[-1]
-    return input, opt, eta, epochs, b_size
+    return inputs, opt, eta, epochs, b_size
 
 
 if __name__ == "__main__":
-    inp, opt, e, epochs, bs = parse_arg()
+    inp, opt, e, epoch, bs = parse_arg()
     # we got the input array now
     size = len(inp)
     x = inp[:(size//2)+1]
     y = inp[size//2:size]
-    model = optim.main()
+    model_optimiser = 'gd'
+    process(model_optimiser)
+
+
---
+++ DL_Models/Library/core/process.py
@@ -0,0 +1,5 @@
+def run_optimiser(*optimiser):
+
+
+if __name__ == "__main__":
+    run_optimiser()
