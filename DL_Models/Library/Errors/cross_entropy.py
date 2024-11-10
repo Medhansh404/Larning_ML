511290dfaab5e11b2696d1fa27c096ec35908d6b
Medhansh404 <21ucs126@lnmiit.ac.in>
2024-11-11 00:09:44 +0530

    Error

--- DL_Models/Library/Errors/cross_entropy.py
+++ DL_Models/Library/Errors/cross_entropy.py
@@ -1,14 +1,13 @@
 import numpy as np
 
+X = []
+Y = []
+
 
 def f(x, w, b):
     return 1 / (1 + np.exp(-(w * x + b)))
 
-
-def error(w, b, X, Y):
+def error(w, b):
     err = 0.0
     for x, y in zip(X, Y):
-        fx = f(x, w, b)
-        err += (fx - y) ** 2
-    return 0.5 * err
-
+        loss = -(f(x) * y + (1-f(x) * (1-y))
